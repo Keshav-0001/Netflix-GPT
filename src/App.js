805c3b301d934c3c9e28c48ef4de12ba451dae99
Keshav Choudhari <keshav09112001@gmail.com>
2024-06-13 18:01:11 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className=" text-red-500 text-[30px] bg-yellow-200">
+      Hii this is keshav Choudhari...
+    </div>
+  );
+}
+
+export default App;
